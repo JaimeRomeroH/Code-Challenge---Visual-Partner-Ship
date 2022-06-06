f64a95ddd9ada527aa1ef729459f8c22b85297ca
JaimeRomero <jaime_im@outlook.com>
2022-06-05 23:01:51 -0500

    Se modifico el archivo index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Reader = require("./lib/utils/Reader");
 const estudiantes = Reader.readJSONfile("estudiantes.json");
 const StudentService = require("./lib/services/StudentService");
+const StudentController = require("./lib/controllers/StudentController");
 //console.log(estudiantes);
-console.log(StudentService.FilterEmailByCertification(estudiantes, true));
+//console.log(StudentService.FilterEmailByCertification(estudiantes, true));
+console.log(StudentController.getEmailByCertification(true));
